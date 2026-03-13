7ba96fcc4271c90b705bd4e8cd341645dc54bce8
yusuke1998 <jhonnyjose1998@gmail.com>
2026-03-12 20:45:48 -0400

    [FIX] SOURCE_DB_TOPIC constant and mypy error

--- src/runboat/constants.py
+++ src/runboat/constants.py
@@ -1,4 +1,4 @@
 """Constants for runboat."""
 
 SOURCE_DB_USE_LAST = "last"  # Value to search for the last DB of the branch/PR
-SOURCE_DB_TOPIC = "runboat_source_db"  # GitHub topic: if repo has it, webhook uses source_db="last" with silent fallback
+SOURCE_DB_TOPIC = "runboat-source-db"  # GitHub topic: if repo has it, webhook uses source_db="last" with silent fallback
